fcbe0e7f77c23615451fcd177ef5435c92fc15f9
DeveshJakhmola16 <dpjakhmola@gmail.com>
2025-11-04 23:56:35 +0530

    Add files via upload

---
+++ Day41Question82.c
@@ -0,0 +1,25 @@
+//Q82: Print each character of a string on a new line.
+
+/*
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+
+*/
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+    int i = 0;
+    scanf("%s", str); // read string 
+    // Print each character on a new line
+    while (str[i] != '\0') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+    return 0;
+}
